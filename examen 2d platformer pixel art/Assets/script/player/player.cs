bdf9498f934cecfa07afe545e55fb1edb965c6d3
rickygr97 <rick.de.graaf@live.nl>
2019-06-24 13:42:23 +0200

    game
    heb erde animaties van de milkshake eringedaan en heb de player script gemaakt voor de movement en het springen en dubble jump

---
+++ examen 2d platformer pixel art/Assets/script/player/player.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    SpriteRenderer komkommer;
+    int speed;
+    Rigidbody2D rb;
+    float moveinput;
+    bool facingleft;
+
+    bool isopgrond;
+    public Transform grondok;
+    public float okradius;
+    public LayerMask grondwatis;
+
+    public int jumpsjumpies;
+    int jumpforce;
+
+    int extrajump;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        komkommer = this.gameObject.GetComponent<SpriteRenderer>();
+        speed = 5;
+        rb = GetComponent<Rigidbody2D>();
+        extrajump = 1;
+        jumpforce = 5;
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isopgrond = Physics2D.OverlapCircle(grondok.position, okradius, grondwatis);
+
+        if(Input.GetKeyDown(KeyCode.W)/*|| Input.GetKeyDown(KeyCode.UpArrow)*/ && jumpsjumpies >0)
+        {
+            rb.velocity = Vector2.up * jumpforce;
+            jumpsjumpies--;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.W) /*|| Input.GetKeyDown(KeyCode.UpArrow)*/ && jumpsjumpies <= 0 && isopgrond == true)
+        {
+            rb.velocity = Vector2.up * jumpforce;
+            
+
+        }
+        if (isopgrond == true)
+        {
+            jumpsjumpies = extrajump;
+
+        }
+
+        #region movement
+        moveinput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveinput * speed, rb.velocity.y);
+        #endregion
+        #region characterleft
+        if (facingleft == true && moveinput < 0)
+        {
+            flipcharacter();
+
+        }
+        if (facingleft == false && moveinput > 0)
+        {
+            flipcharacter();
+
+        }
+        #endregion
+        #region firstwhatif
+
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.position += Vector3.left * speed * Time.deltaTime;
+        //    komkommer.flipX = false;
+
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.position += Vector3.right * speed * Time.deltaTime;
+        //    komkommer.flipX = true;
+        #endregion
+
+        //}
+    }
+    void flipcharacter()
+    {
+        facingleft = !facingleft;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+
+    }
+}
