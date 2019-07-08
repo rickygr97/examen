736523032c0a4aba312643421a9a1ab0513b92bd
rickygr97 <rick.de.graaf@live.nl>
2019-07-08 16:45:53 +0200

    game
    je levels worden opgeslagen

---
+++ examen 2d platformer pixel art/Assets/script/systems/levelsunlock.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelsunlock : MonoBehaviour
+{
+    public GameObject level2;
+    public GameObject level3;
+    public static int nummer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nummer = 1;
+        if (PlayerPrefs.HasKey("nummer"))
+        {
+            nummer = PlayerPrefs.GetInt("nummer");
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (nummer)
+        {
+            case 1:
+                break;
+            case 2:
+                level2.active = false;
+                break;
+            case 3:
+                level3.active = false;
+                break;
+
+        }
+    }
+    public static void nummers(int nummererbij)
+    {
+        nummer += nummererbij;
+
+    }
+}
