5f10a1f36a8e5aec13ff2325f3e2d9d162105b73
rickygr97 <rick.de.graaf@live.nl>
2019-07-10 15:46:37 +0200

    added
    added some music and the ebilety to pause the game end unpause by pressing escape buttons

--- examen 2d platformer pixel art/Assets/script/systems/pause.cs
+++ examen 2d platformer pixel art/Assets/script/systems/pause.cs
@@ -8,24 +8,37 @@ public class pause : MonoBehaviour
 {
     public Canvas canvaspause;
     public Canvas canvasmain;
+    bool pausese;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         canvaspause.enabled = false;
+        pausese = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape)&& pausese)
         {
             Time.timeScale = 0;
             canvaspause.enabled = true;
             canvasmain.enabled = false;
+            pausese = false;
 
 
+
+
+        }else if(Input.GetKeyDown(KeyCode.Escape)&& pausese == false)
+        {
+            Time.timeScale = 1;
+            canvaspause.enabled = false;
+            canvasmain.enabled = true;
+            pausese = true;
 
         }
     }
@@ -41,6 +54,12 @@ public class pause : MonoBehaviour
     {
         Time.timeScale = 1;
         SceneManager.LoadScene(0);
+
+    }
+    public void option()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(2);
 
     }
 }
